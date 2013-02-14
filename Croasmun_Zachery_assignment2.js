28ef26a68e0b7503b45965793c093954e7e44b30
Zachery croasmun <th3p4ler1der@gmail.com>
2013-02-14 14:21:10 -0800

    first update

--- Croasmun_Zachery_assignment2.js
+++ Croasmun_Zachery_assignment2.js
@@ -1,1 +1,22 @@
-alert("JavaScript works!");
+alert("The story of the 4 Brave Paladens!");
+
+//functions
+
+var paladenNames = ["Sam", "tim", "jake", "Bob"];
+var minuetsPerSpell = [10, 25, 33, 65];
+
+var killOneEnimy = function(paladenNumber){
+	var paladenName = paladenNames [paladenNumber];
+		minOfSpell = minuetsPerSpell [paladenNumber];
+	console.log("It will take the mighty " + paladenName + " a total of " + minOfSpell + " Minuets to cast the death curse!");
+	
+	for (var minuets = 0; minuets < minOfSpell; minuets += 2){
+	var minLeft = minOfSpell - minuets;
+	console.log(minuets + " have passed " + minLeft + " till he is finished");
+	};
+	
+};
+killOneEnimy(0);
+killOneEnimy(1);
+killOneEnimy(2);
+killOneEnimy(3);
