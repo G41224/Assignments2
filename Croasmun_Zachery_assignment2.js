dca7ebb330dbf59498e93a9c67cc1ca00ede7459
Zachery croasmun <th3p4ler1der@gmail.com>
2013-02-14 15:37:05 -0800

    second update and some small changes

--- Croasmun_Zachery_assignment2.js
+++ Croasmun_Zachery_assignment2.js
@@ -1,18 +1,21 @@
-alert("The story of the 4 Brave Paladens!");
+alert("The story of 4 Brave Wizards!");
 
 //functions
 
-var paladenNames = ["Sam", "tim", "jake", "Bob"];
-var minuetsPerSpell = [10, 25, 33, 65];
+var wizardNames = ["Sam", "tim", "jake", "Bob"],
+	minuetsPerSpell = [4, 14, 8, 12],
+	dragonNames = ["Thor", "Thunder", "Claw", "Chaos"];
 
-var killOneEnimy = function(paladenNumber){
-	var paladenName = paladenNames [paladenNumber];
-		minOfSpell = minuetsPerSpell [paladenNumber];
-	console.log("It will take the mighty " + paladenName + " a total of " + minOfSpell + " Minuets to cast the death curse!");
+var killOneEnimy = function(wizardNumber){
+	var wizardName = wizardNames [wizardNumber];
+		minOfSpell = minuetsPerSpell [wizardNumber];
+		dragon = dragonNames [wizardNumber];
+	console.log("It will take the mighty " + wizardName + " a total of " + minOfSpell + " Minuets to cast the death curse!"
+	+ " against the terrifyingly fearsome " + dragon);
 	
-	for (var minuets = 0; minuets < minOfSpell; minuets += 2){
-	var minLeft = minOfSpell - minuets;
-	console.log(minuets + " have passed " + minLeft + " till he is finished");
+	for(var minuets = 0; minuets < minOfSpell; minuets += 2){
+		var minLeft = minOfSpell - minuets;
+		console.log(minuets + " Minuets have passed " + minLeft + " Minuets till he is finished");
 	};
 	
 };
@@ -20,3 +23,28 @@ killOneEnimy(0);
 killOneEnimy(1);
 killOneEnimy(2);
 killOneEnimy(3);
+
+//story line in here
+console.log();
+
+
+var pathToTake = ["North", "South", "East", "West"],
+	daysOnPath = [8, 7, 9, 3];
+	
+var whichPath = function(pathNumber){
+	var path = pathToTake [pathNumber];
+		days = daysOnPath [pathNumber];
+		wizard = wizardNames [pathNumber];
+	console.log(wizard + " decided upon taking the path to the " + path + " and walked for " + days);
+	
+	for(var daysOfTravel = 0; daysOfTravel < days; daysOfTravel += 1){
+		var daysLeft = daysOfTravel - days;
+		console.log(daysOfTravel + " Days Traveled! " + daysLeft + " Days left on this journey! ");	
+	};
+
+};
+
+whichPath(0);
+whichPath(1);
+whichPath(2);
+whichPath(3);
