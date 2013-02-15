fc8697c0eb2bff8492d464f5796c95c3c8567a6a
Zachery croasmun <th3p4ler1der@gmail.com>
2013-02-14 18:14:28 -0800

    another update including html

--- Croasmun_Zachery_assignment2.js
+++ Croasmun_Zachery_assignment2.js
@@ -1,6 +1,6 @@
 alert("The story of 4 Brave Wizards!");
 
-//functions
+console.log("Our story begins as our 4 hero’s are finishing off a fight against dragons.");
 
 var wizardNames = ["Sam", "tim", "jake", "Bob"],
 	minuetsPerSpell = [4, 14, 8, 12],
@@ -11,13 +11,13 @@ var killOneEnimy = function(wizardNumber){
 		minOfSpell = minuetsPerSpell [wizardNumber];
 		dragon = dragonNames [wizardNumber];
 	console.log("It will take the mighty " + wizardName + " a total of " + minOfSpell + " Minuets to cast the death curse!"
-	+ " against the terrifyingly fearsome " + dragon);
+	+ " against the terrifyingly fearsome dragon " + dragon);
 	
 	for(var minuets = 0; minuets < minOfSpell; minuets += 2){
 		var minLeft = minOfSpell - minuets;
-		console.log(minuets + " Minuets have passed " + minLeft + " Minuets till he is finished");
+		console.log(minuets + " Minuets have passed " + minLeft + " Minuets till spell is finished");
 	};
-	
+	return console.log("I have slain the beast!");
 };
 killOneEnimy(0);
 killOneEnimy(1);
@@ -39,12 +39,42 @@ var whichPath = function(pathNumber){
 	
 	for(var daysOfTravel = 0; daysOfTravel < days; daysOfTravel += 1){
 		var daysLeft = daysOfTravel - days;
-		console.log(daysOfTravel + " Days Traveled! " + daysLeft + " Days left on this journey! ");	
+		console.log(daysOfTravel + " Days Traveled! " + daysLeft + " Days left on his journey! ");	
 	};
-
+	return console.log("We made it alive!");
 };
 
 whichPath(0);
 whichPath(1);
 whichPath(2);
 whichPath(3);
+
+
+// more story line here
+console.log();
+
+var spellToUse = ["Death", "Paralyses", "Fear", "Shielding"],
+	spellTime = [8, 4, 6, 2];
+	
+var spellUsed = function(spellNumber){
+	var spell = spellToUse [spellNumber];
+		timeOfSpell = spellTime [spellNumber];
+		nameOfWizard = wizardNames [spellNumber];
+	console.log(nameOfWizard + " cast the spell of " + spell + " and it took him " + timeOfSpell + "minuets!");
+
+	for(var minToSpell = 0; minToSpell < timeOfSpell; minToSpell += 1){
+		var spellUsed = timeOfSpell - minToSpell;
+		console.log(minToSpell + " Minuets passed " + spellUsed + " Minuets are left.");
+	
+	};		
+	return console.log("Spell Cast!");
+
+};
+
+spellUsed(0);
+spellUsed(1);
+spellUsed(2);
+spellUsed(3);
+
+//end of story
+Console.log();
